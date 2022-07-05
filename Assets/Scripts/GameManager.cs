dacedb39b89359cfa1eb53e1bd0edb14994f98ad
pudding <talesw06992@gmail.com>
2022-07-05 17:58:34 +0800

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tanks
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        public static GameManager instance;
+        string gameVersion = "1";
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Debug.LogErrorFormat(gameObject,
+                "Multiple instances of {0} is not allow", GetType().Name);
+                DestroyImmediate(gameObject);
+                return;
+            }
+            PhotonNetwork.AutomaticallySyncScene = true;
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    // Start is called before the first frame update
+    void Start()
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        public override void OnConnected()
+        {
+            Debug.Log("PUN Connected");
+        }
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Connected to Master");
+        }
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
+        }
+
+        public void JoinGameRoom()
+        {
+            var options = new RoomOptions
+            {
+                MaxPlayers = 6
+            };
+            PhotonNetwork.JoinOrCreateRoom("Kingdom", options, null);
+        }
+
+        public override void OnCreatedRoom()
+        {
+            Debug.Log("Created room!!");
+        }
+
+        public override void OnJoinedRoom()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.Log("Created room!!");
+                PhotonNetwork.LoadLevel("GameScene");
+            }
+            else
+            {
+                Debug.Log("Joined room!!");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
